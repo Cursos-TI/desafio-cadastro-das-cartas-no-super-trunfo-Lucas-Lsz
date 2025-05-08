aca69dce3cbe299236bd24508cf9a53f534a12da
Lucas-Lsz <lucas.lsz2004@gmail.com>
2025-05-08 13:15:17 +0000

    Criei variaveis do sistema de cartas supertrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+// Carta 1
+char estado1;
+char codigo1[5];
+char cidade1[50];
+int populacao1;
+float area1;
+float pib1;
+int pontosTuristicos1;
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// Carta 2
+char estado2;
+char codigo2[5];
+char cidade2[50];
+int populacao2;
+float area2;
+float pib2;
+int pontosTuristicos2;
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
